5c869ee0f6758a97baf99d4211edd7b1c1d2d9c0
Bryant Wang and Eric Raetz <bryant@>
2016-10-17 10:37:49 -0700

    Add 6 conversion functions.

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var gallonToLiter = function(gallon) {
+  return gallon * 3.785411784;
+};
+
+var literToGallon = function(liter) {
+  return liter / 3.785411784;
+};
+
+var cupToLiter = function(cup) {
+  return cup * 0.2365882365;
+};
+
+var literToCup = function(liter) {
+  return liter / 0.2365882365;
+};
+
+var ozToMl = function(oz) {
+  return oz * 29.5735296;
+};
+
+var mlToOz = function(ml) {
+  return ml / 29.5735296;
+};
